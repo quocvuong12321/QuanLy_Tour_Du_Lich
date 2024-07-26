8a75ab2fd375603b0131f6434a0787b4e98b9d05
Nguyen Vuong <nguyenquocvuong12321@gmail.com>
2024-07-26 19:49:45 +0700

    first commit

---
+++ QuanLy_TourDuLich/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QuanLy_TourDuLich.Models;
+namespace QuanLy_TourDuLich.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        QuanLyTourDuLichDataContext data = new QuanLyTourDuLichDataContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
