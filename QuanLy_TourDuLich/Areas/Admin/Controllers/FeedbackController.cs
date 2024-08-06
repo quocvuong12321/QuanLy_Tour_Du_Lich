e8050ebd312530b90a7a3cbf1f52f80d9e1392a1
Samson451 <117883706+Samson451@users.noreply.github.com>
2024-08-06 15:33:03 +0700

    Chinh sua layout va them feedback

---
+++ QuanLy_TourDuLich/Areas/Admin/Controllers/FeedbackController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QuanLy_TourDuLich.Models;
+namespace QuanLy_TourDuLich.Areas.Admin.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        CSDL_QLTourDataContext data = new CSDL_QLTourDataContext();
+        public ActionResult Index()
+        {
+            var users = data.Feedbacks.ToList();
+            return View(users);
+        }
+        public ActionResult Details(int id) 
+        {
+            var fb = data.Feedbacks.FirstOrDefault(t => t.id == id);
+            return View(fb); 
+        }
+
+        public ActionResult Delete(int id)
+        {
+            data.Feedbacks.DeleteOnSubmit(data.Feedbacks.FirstOrDefault(t => t.id == id));
+            data.SubmitChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
+    
